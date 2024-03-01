4bcf165128d9c5e45a5590ff8f993274d14b9180
Musfira Murtaza <120357757+musfiramurtaza@users.noreply.github.com>
2024-03-01 10:26:46 +0500

    Create Dummy Data exercise01.moongodb.js

---
+++ Dummy Data exercise01.moongodb.js
@@ -0,0 +1,48 @@
+//EXERCISE
+
+var Company = [
+    {
+        name: "Aliza",
+        salary: 4500000000,
+        language: "English",
+        city "Karachi",
+        isCoder: true
+    },
+        {
+        name: "Ali",
+        salary: 500000,
+        language: "Urdu",
+        city: "Lahore",
+        isCoder: false
+    },
+    {
+        name: "Fatima",
+        salary: 600000,
+        language: "Arabic",
+        city: "Islamabad",
+        isCoder: true
+    },
+    {
+        name: "Minal",
+        salary: 550000,
+        language: "English",
+        city: "Karachi",
+        isCoder: false
+    },
+    {
+        name: "Minahil",
+        salary: 520000,
+        language: "Urdu",
+        city: "Lahore",
+        isCoder: true
+    },
+    {
+        name: "Iqra",
+        salary: 510000,
+        language: "Punjabi",
+        city: "Islamabad",
+        isCoder: false
+    },
+];
+
+db.Company.insertMany(companyEmployees);
